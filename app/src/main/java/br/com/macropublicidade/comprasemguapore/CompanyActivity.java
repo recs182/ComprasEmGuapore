39e9d6d11622bbb2890b8379fa443695cac59235
Renan Soares <renan-csoares@hotmail.com>
2016-09-06 23:59:04 -0300

    layout views, listeners

--- app/src/main/java/br/com/macropublicidade/comprasemguapore/CompanyActivity.java
+++ app/src/main/java/br/com/macropublicidade/comprasemguapore/CompanyActivity.java
@@ -2,6 +2,8 @@ package br.com.macropublicidade.comprasemguapore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,7 +13,7 @@ import br.com.macropublicidade.comprasemguapore.models.Company;
 import br.com.macropublicidade.comprasemguapore.models.Group;
 import br.com.macropublicidade.comprasemguapore.models.Sector;
 
-public class CompanyActivity extends AppCompatActivity {
+public class CompanyActivity extends AppCompatActivity implements View.OnClickListener{
 
     private String[] url_banner = new String[]{"http://www.comprasemguapore.com.br/images/companies/banners/", "_banner.jpg"};
     private String[] url_logo = new String[]{"http://www.comprasemguapore.com.br/images/companies/logos/", "_logo.jpg"};
@@ -36,6 +38,16 @@ public class CompanyActivity extends AppCompatActivity {
         TextView phone = (TextView) findViewById(R.id.CompanyActivity_phone);
         TextView description = (TextView) findViewById(R.id.CompanyActivity_description);
 
+        Button website = (Button) findViewById(R.id.CompanyActivity_webpage);
+        Button facebook = (Button) findViewById(R.id.CompanyActivity_facebook);
+        Button instagram = (Button) findViewById(R.id.CompanyActivity_instagram);
+        Button whatsapp = (Button) findViewById(R.id.CompanyActivity_whatsapp);
+
+        website.setOnClickListener(this);
+        facebook.setOnClickListener(this);
+        instagram.setOnClickListener(this);
+        whatsapp.setOnClickListener(this);
+
         //TODO: download banner to phone, check if image exists in phone and load it
         //TODO: load logos in ListCompaniesActivity jhjkh
         Picasso.with(this).load(url_banner[0] + company.getId().toString() + url_banner[1]).fit().into(banner);
@@ -48,14 +60,48 @@ public class CompanyActivity extends AppCompatActivity {
             sector_name.setText( group.getName() );
         }
 
-        this.textViewValue( description, company.getDescription() );
+        this.textviewText( description, company.getDescription() );
+
+        this.buttonVisibility( website, company.getWebpage() );
+        this.buttonVisibility( facebook, company.getFacebook() );
+        this.buttonVisibility( instagram, company.getInstagram() );
+        this.buttonVisibility( whatsapp, company.getWhatsapp() );
     }
 
-    private void textViewValue( TextView view, String value ){
-        if( value.length() > 0 ){
-            view.setText( value );
-        }else{
+    private void textviewText( TextView view, String value ){
+        String tested = this.preventNullValue( value );
+        if( tested.isEmpty() ){
             view.setVisibility(TextView.GONE);
+        }else{
+            view.setText( value );
+        }
+    }
+
+    private void buttonVisibility( Button btn, String value ){
+        String tested = this.preventNullValue( value );
+        if( tested.isEmpty() ){
+            btn.setVisibility(Button.GONE);
+        }
+    }
+
+    private String preventNullValue( String value ){
+        if( value == null || value.equals("null") ) {
+            return "";
+        }
+        return value;
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.CompanyActivity_webpage:
+                break;
+            case R.id.CompanyActivity_facebook:
+                break;
+            case R.id.CompanyActivity_instagram:
+                break;
+            case R.id.CompanyActivity_whatsapp:
+                break;
         }
     }
 
